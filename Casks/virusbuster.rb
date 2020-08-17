abe6520dbcdb19f8aef131312e26389572d39157
psfldr <psfldr@gmail.com>
2020-08-17 11:10:15 +0900

    OSの指定はreleaseではなくversion

--- Casks/virusbuster.rb
+++ Casks/virusbuster.rb
@@ -1,5 +1,5 @@
 cask "virusbuster" do
-  if MacOS.release >= :catalina
+  if MacOS.version >= :catalina
     version '10.0.1686'
     sha256 '1ffc79ff269a1ad6213f377e8411e773f97e3b5080197749da0790db7c3e82ca'
     url "https://files.trendmicro.com/products/iTIS/10.0/GM3/jp/Virus%20Buster%20for%20Mac-#{version}.dmg"
